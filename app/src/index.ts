3a0b88d077795d1e44e5e1a85946e9ac58f0f2b3
thompsonjessea <thompsonjessea@gmail.com>
2022-04-06 18:09:13 -0500

    Added mongo

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express'
 import {json} from 'body-parser'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
+dotenv.config()
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://mongo:27017", () => {
+    console.log('Connected to mongo')
+})
 const app = express()
 app.use(json())
 
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 } );
-app.listen(3000, () => {
-    console.log('server listening on port 3000')
+app.listen(PORT, () => {
+    console.log(`server listening on port ${PORT}`)
 })
